16aa4966e81cfb85c312807742b15a43853072c8
webdaksh <webdaksh1@gmail.com>
2026-04-05 11:43:36 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I’m Daksh , a 19 year old student focused on development and design. Currently, <br/> I'm building my skills in web development, video editing, front end developing, to create responsive user interface,
+          I’m Daksh,a student who focused on development and design. Currently, <br/> I'm building my skills in web development, video editing, front end developing, to create responsive user interface,
         </p>
       </div>
     </div>
